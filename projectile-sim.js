9141d3ed5902af60e532b1f210b5e0eac277f6c1
Trey Werr <treywerr@gmail.com>
2021-02-03 11:34:07 -0600

    Initial slider functionality for projectile sim

--- projectile-sim.js
+++ projectile-sim.js
@@ -2,8 +2,7 @@
 /* ----- Variables and Constants ------- */
 const G = 9.8; // acceleration due to gravity
 var theta = Math.PI/4; // the launch angle measured in radians
-// TODO the slider for theta is labelled in degrees. Convert this value to rads oninput
-var v0 = 10; // initial velocity
+var v = 10; // initial velocity
 
 /* ------ Projectile Path Methods ------- */
 
@@ -13,14 +12,14 @@ var v0 = 10; // initial velocity
  * @return {number} The vertical position of the projectile.
  */
 function projectileHeight(x) {
-    return ( Math.tan(theta)*x) - ( (G*x*x) / (2*v0*v0*Math.cos(theta)*Math.cos(theta)) );
+    return ( Math.tan(theta)*x) - ( (G*x*x) / (2*v*v*Math.cos(theta)*Math.cos(theta)) );
 }
 
 /**
  * Calculates where the projectile will land given an initial velocity and launch angle.
  * @return {number} The landing distance of the projectile.
 function landingDistance() {
-    return v0*v0*Math.sin(2*theta)/G;
+    return v*v*Math.sin(2*theta)/G;
 }
 
 /**
@@ -28,7 +27,7 @@ function landingDistance() {
  * @return {number} The horizontal range of the projectile in meters.
  */
 function horizontalRange() {
-    return v0*v0/G;
+    return v*v/G;
 }
 
 /**
@@ -36,26 +35,23 @@ function horizontalRange() {
  * @return {number} The vertical range of the projectile in meters.
  */
 function verticalRange() {
-    return (v0*v0) / (2*G);
+    return (v*v)/(2*G);
 }
 
 var numSteps = 100;
 var stepSize = horizontalRange()/numSteps;
 var xCoords = [];
 var yCoords = [];
-for (var i = 0; i <= numSteps; i++) {
-    xCoords[i] = i*stepSize;
-    yCoords[i] = projectileHeight(xCoords[i]);
-}
+updateProjectilePath();
 
 
 /* -------------- Initial Graph Setup ---------------- */
 
-var projectile = {
+var projectile;/* = {
     x: xCoords,
     y: yCoords,
     mode: 'lines'
-};
+};*/
 
 var data = [
     projectile
@@ -81,3 +77,66 @@ var layout = {
 
 graph = document.getElementById('graph');
 Plotly.newPlot(graph, data, layout);
+
+/* ------------Update Graph On Input-------------- */
+
+
+/* ---------oninput functions---------- */
+
+var velocityMax = 20;
+var velocitySlider = document.getElementById('velocitySlider');
+velocitySlider.oninput = function() {
+    v = velocityMax*(velocitySlider.value/100);
+    updateGraph();
+}
+
+var angleMax = Math.PI/2;
+var angleSlider = document.getElementById('angleSlider');
+angleSlider.oninput = function() {
+    theta = angleMax*(angleSlider.value/100);
+    updateGraph();
+}
+
+
+/* ---------trace update functions--------- */
+
+/**
+ * Updates the path of the projectile.
+ */
+function updateProjectilePath() {
+    stepSize = horizontalRange()/numSteps;
+    xCoords = [];
+    yCoords = [];
+
+    for (var i = 0; i <= numSteps; i++) {
+        xCoords[i] = i*stepSize;
+        yCoords[i] = projectileHeight(xCoords[i]);
+    }
+
+    projectile = {
+        x: xCoords,
+        y: yCoords,
+        mode: 'lines'
+    };
+}
+
+/**
+ * Updates all elements on the graph to reflect changes caused by user input.
+ */
+function updateGraph() {
+    
+    updateProjectilePath();
+    
+    refreshGraph();
+}
+
+/**
+ * Refreshes the data array and calls Plotly.react to make changes appear on graph.
+ */
+function refreshGraph() {
+    data = [
+        projectile
+    ];
+
+    Plotly.react(graph, data, layout);
+}
